c871032dee668bd568c14806e4a8d937b6917f0c
Marian Poeschmann <github@mail.itsmethemojo.eu>
2023-08-26 01:18:07 +0200

    set private ip when registering nodes

--- terraform/scripts/init_worker.sh
+++ terraform/scripts/init_worker.sh
@@ -3,6 +3,7 @@
 node_postfix=$(date '+%Y-%m-%d-%H-%M') && \
 private_ip=$(ifconfig | grep 'inet ' | grep ${IP_FILTER} | awk '{print $2}') && \
 curl -sfL https://get.k3s.io | K3S_NODE_NAME=${K3S_NODE_NAME}-$node_postfix K3S_TOKEN=${K3S_TOKEN} sh -s - agent \
+  --node-ip $private_ip \
   --server https://${MASTER_IP}:6443 \
   --kubelet-arg="cloud-provider=external" \
   --kubelet-arg="provider-id=hcloud://$private_ip"
